219ffcf0efcc69b02d15057f1aa5073c8af6b7d6
LanceGao <1123764761@qq.com>
2018-12-08 00:09:47 +0800

    使用axios请求seller数据

--- vue-sell-demo/src/App.vue
+++ vue-sell-demo/src/App.vue
@@ -6,9 +6,28 @@
 
 <script>
 import VHeader from 'components/v-header/v-header.vue'
+import { getSeller } from 'api'
 
 export default {
   name: 'app',
+  data() {
+    return {
+      seller: {}
+    }
+  },
+  created() {
+    console.log('created', this)
+    this._getSeller()
+  },
+  methods: {
+    _getSeller() {
+      getSeller().then((data) => {
+        console.log('methods', this)
+        this.seller = data
+        console.log(this.seller)
+      })
+    }
+  },
   components: {
     VHeader
   }
